a6133a071ff1c8b00b2118d885b154297693e82f
tgoikawa <kosuke.oikawa@topgate.co.jp>
2020-01-05 22:25:33 +0900

    modified argument type.

--- src/csv_src.rs
+++ src/csv_src.rs
@@ -1,9 +1,8 @@
 use crate::*;
-use std::io::prelude::*;
 use std::io::Cursor;
 
-pub(crate) fn read_csv<R: Read>(reader: R) -> Result<Vec<Word>, Error> {
-    let mut rdr = csv::Reader::from_reader(reader);
+pub(crate) fn read_csv(body: &str) -> Result<Vec<Word>, Error> {
+    let mut rdr = csv::Reader::from_reader(Cursor::new(body));
     let returns = rdr.deserialize::<Word>().collect::<Result<Vec<_>, _>>()?;
     Ok(returns)
 }
@@ -24,16 +23,9 @@ mod tests {
     #[test_case(&[] ,vec![]; "nothing rows")]
     fn read_csv_works(given: &RowArray, expected: Vec<Word>) -> Result<(), Error> {
         let src = &with_header(given);
-        let cursor = Cursor::new(src);
-        let actulal = read_csv(cursor)?;
+        let actulal = read_csv(src)?;
         assert_eq!(&expected[..], &actulal[..]);
         Ok(())
-    }
-    #[test_case("read io error occured")]
-    fn read_csv_error_works(expected: &str) {
-        let result = read_csv(&mut MockErrorRead {});
-        assert!(result.is_err());
-        assert_eq!(expected, format!("{}", result.err().unwrap()));
     }
 
     fn with_header(rows: &RowArray) -> String {
@@ -43,16 +35,5 @@ mod tests {
             .map(|rows| rows.join(","))
             .collect::<Vec<_>>()
             .join("\n")
-    }
-
-    struct MockErrorRead;
-    use std::io;
-    impl Read for MockErrorRead {
-        fn read(&mut self, _: &mut [u8]) -> io::Result<usize> {
-            Err(io::Error::new(
-                io::ErrorKind::Other,
-                "read io error occured",
-            ))
-        }
     }
 }
